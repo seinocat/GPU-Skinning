4f5abbf695f2e29db0cdaf80ed713df558e39575
songqin <980047129@qq.com>
2024-01-04 14:11:25 +0800

    first commit

---
+++ Unity/Assets/GPU-Skinning/Runtime/Baker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Seino.GpuSkinning.Runtime
+{
+    public class Baker : MonoBehaviour
+    {
+        public Animator Animator;
+
+        private void Awake()
+        {
+            this.Animator = GetComponent<Animator>();
+        }
+
+
+        [Button("烘焙")]
+        public void Bake()
+        {
+
+        }
+    }
+}
