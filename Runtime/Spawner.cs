3fc6f9b1da4e07aa51e15e6aced1ca8714755ffa
songqin <980047129@qq.com>
2024-01-04 14:11:25 +0800

    first commit

---
+++ Runtime/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace GPU_Skinning.Runtime
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject Model;
+        
+        private void Start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Instantiate(Model, new Vector3(i, 1.06f, j), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
